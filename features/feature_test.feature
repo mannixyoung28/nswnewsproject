1886b6411d9b644ecc4241b9257b3d36b794fc3e
mannixyoung28 <mannix.young@gmail.com>
2021-04-29 16:27:36 +1000

    Automation Task 2
    Automation Task 2.  1st test will pass. 2nd test will fail

--- features/feature_test.feature
+++ features/feature_test.feature
@@ -5,6 +5,19 @@ Feature: News Topic Filter
   @SmokeTest
   Scenario: Verify 1 topic filter functionality
     Given I open "https://www.nsw.gov.au/news"
+    When I click on Filter
     When I select topics "Awards"
     And I click on Apply
-    Then I verify "Awards" topic filter results
+    Then I verified "Awards" topic filter results
+
+ @SmokeTest
+  Scenario: Verify topic filter reset functionality
+    Given I open "https://www.nsw.gov.au/news"
+    When I click on Filter
+    When I select topics "Awards"
+    And I select topics "Business and Economy"
+    And I select topics "Education"
+    And I click on Apply
+    And I click on Reset
+    And I click on Filter
+    Then I verified that filter is cleared
--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -32,18 +32,30 @@ Given("I open {string}", {
   await news_page.goto(url);
 });
 
+
+When("I click on Filter", {
+  timeout: 10 * 5000
+}, async function () {
+  var news_page = new News();
+  
+  //await browser.sleep(5000);
+  //console.log("sleep");
+
+  // Expand the filter
+  var EC = protractor.ExpectedConditions;
+  await browser.wait(EC.elementToBeClickable(news_page.filterTopicButton), 30000);
+  await news_page.scrollIntoView(news_page.filterTopicButton);
+  await news_page.filterTopicButton.click();
+  console.log("filter clicked");
+});
+
+
 When("I select topics {string}", {
   timeout: 10 * 5000
 }, async function (topicName) {
   var news_page = new News();
 
-  // Expand the filter
-  var EC = protractor.ExpectedConditions;
-  await browser.wait(EC.visibilityOf(news_page.filterTopicButton), 30000);
-  await news_page.filterTopicButton.click();
-  console.log("filter clicked");
-
-  // Select a filter topic
+   // Select a filter topic
   var topicFilter = element(by.cssContainingText("label", topicName));
   await browser.wait(EC.visibilityOf(topicFilter), 20000);
   await news_page.scrollIntoView(topicFilter);
@@ -63,7 +75,7 @@ When("I click on Apply", {
 });
 
 
-Then("I verify {string} topic filter results", {
+Then("I verified {string} topic filter results", {
   timeout: 6 * 5000
 }, async function (topicName) {
   var EC = protractor.ExpectedConditions;
@@ -71,6 +83,37 @@ Then("I verify {string} topic filter results", {
     // Will print 0 First, 1 Second, 2 Third.
     child.element(by.css('p.nsw-card__tag')).getText().then(async function (text) {
       await expect(topicName).to.equal(text);
+    });
+  });
+});
+
+
+When("I close the browser", {
+  timeout: 10 * 5000
+}, async function () {
+  await browser.close();
+});
+
+When("I click on Reset", {
+  timeout: 6 * 5000
+}, async function () {
+  var EC = protractor.ExpectedConditions;
+  // Click on Apply button
+  var resetButton = element(by.css("input[value=\"Reset\"]"));
+  await browser.wait(EC.visibilityOf(resetButton), 15000);
+  await resetButton.click();
+  console.log("Reset clicked");
+});
+
+Then("I verified that filter is cleared", {
+  timeout: 6 * 5000
+}, async function () {
+  timeout: 6 * 5000
+  var EC = protractor.ExpectedConditions;
+  element.all(by.css("input[type=\"checkbox\"]")).each(function(child, index) {
+      child.getText().then(async function (value) {
+      console.log("value is " + value);
+      await expect(value).to.equal("");
     });
   });
 });
